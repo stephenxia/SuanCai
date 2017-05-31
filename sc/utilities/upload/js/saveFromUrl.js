df7a1bd7ef6e30567b7f7b765f618206230a41fe
moonlightlane <wangzichao6@gmail.com>
2017-05-30 22:24:51 -0500

    minor update

--- sc/utilities/upload/js/saveFromUrl.js
+++ sc/utilities/upload/js/saveFromUrl.js
@@ -15,17 +15,22 @@ function saveFromUrl() {
     var input_url = document.getElementById("upload-url").value,// Download a file form a url.
     
     // error div setup for displaying error message when input is not a url
-        errorDiv = document.getElementsByClassName("uploadError")[0];
+        errorDiv  = document.getElementsByClassName("uploadError")[0];
     
     // check whether the input is a valid url; if not, display error message
     // TODO (at this time just assume the input is a valid url)
     
     // download file to specific location
-    var link = document.createElement('a');
-    link.href = input_url;
-    link.download = '../userStoage/file.pdf'; // specific location. WARNING: might not work
+    var link      = document.createElement('a'),
+        filename  = input_url.substring(input_url.lastIndexOf("/") + 1).split("?")[0];
+    link.setAttribute('id', 'temp');
+    link.href     = input_url;
+    link.download = '../userStoage/'.concat(filename); // specific location. WARNING: might not work
     link.dispatchEvent(new MouseEvent('click'));
+    document.getElementById("temp").remove();
 }
+
+
 
 // unit test: whether the error message display works 
 function test() {
